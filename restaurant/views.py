862956230a44872eb694bb7c19eb0be669d3708c
Lucas Santana Lemos <lslemos59@gmail.com>
2023-10-31 15:50:23 -0300

    #

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from .models import *
 
 def home(request):
+    meals = Meal.objects.filter(
+        is_published=True,
+    ).order_by('-id')
+    
     context = {
+        'meals': meals,
         'title': 'Home'
     }
     
