7b04114ed146d09b0cce42dd8ca3fcc686da8207
Lucas Santana Lemos <lslemos59@gmail.com>
2023-11-01 11:54:29 -0300

    #

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -9,6 +9,7 @@ app_name = 'restaurant'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+     path('meal-page/<int:id>', views.meal, name='meal-page'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import *
+
 
 def home(request):
     meals = Meal.objects.filter(
@@ -12,3 +13,18 @@ def home(request):
     }
     
     return render(request, 'global/partials/home.html', context)
+
+
+def meal(request, id):
+    meals = get_object_or_404(
+        Meal,
+        pk = id,
+        is_published = True
+    )
+    
+    context = {
+        'title': 'Meal',
+        'meals': meals
+    }
+    
+    return render(request, 'restaurant/pages/meal-page.html', context)
